786367ac197590445e87da42c827000e8ce97a48
Wyrmi <35914703+Wyrmi@users.noreply.github.com>
2018-10-03 17:44:35 +0300

    Update NewPlayer.cs

--- Leaderboard/Models/NewPlayer.cs
+++ Leaderboard/Models/NewPlayer.cs
@@ -4,8 +4,11 @@ namespace Leaderboard.Models
 {
     public class NewPlayer
     {
-        
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
         public string Name {get; set;}
+        [Required]
+        [Range(1,999)]
         public int Score {get; set;}
     }
 }
